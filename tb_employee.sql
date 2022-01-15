344053c8b88476c296b3211b600cb83ecb847fd9
Damar Mustiko Aji <damarresin@gmail.com>
2022-01-15 15:23:01 +0700

    adding sql file
    data table for this project

---
+++ tb_employee.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Dec 31, 2021 at 11:37 AM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `indonetsource`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_employee`
+--
+
+CREATE TABLE `tb_employee` (
+  `id` int(11) NOT NULL COMMENT 'primary key',
+  `employee_name` varchar(255) NOT NULL COMMENT 'employee name',
+  `employee_salary` double NOT NULL COMMENT 'employee salary',
+  `employee_age` int(11) NOT NULL COMMENT 'employee age'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='datatable demo table';
+
+--
+-- Dumping data for table `tb_employee`
+--
+
+INSERT INTO `tb_employee` (`id`, `employee_name`, `employee_salary`, `employee_age`) VALUES
+(2, 'damar edit', 23421, 34),
+(64, 'damar edit', 23421, 34),
+(67, 'asd', 1232323, 12),
+(68, 'asd', 1232323, 12);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_employee`
+--
+ALTER TABLE `tb_employee`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_employee`
+--
+ALTER TABLE `tb_employee`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'primary key', AUTO_INCREMENT=69;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
