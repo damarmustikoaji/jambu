f1a79df103da2cff561208874f2ea09391cc3559
damar <damar_aji@linkaja.id>
2022-02-04 14:20:38 +0700

    updated

--- connection.php
+++ connection.php
@@ -1,4 +1,19 @@
 <?php
-Class dbObj { var $servername = "localhost"; var $username = "root"; var $password = ""; var $dbname = "indonetsource"; var $conn;
-function getConnstring() { $con = mysqli_connect($this->servername, $this->username, $this->password, $this->dbname) or die("Connection failed: " . mysqli_connect_error()); if (mysqli_connect_errno()) {printf("Connect failed: %s\n", mysqli_connect_error());exit();} else {$this->conn = $con;} return $this->conn;}}
+Class dbObj { 
+    var $servername = "us-cdbr-east-05.cleardb.net"; 
+    var $username = "b43f42459eebe7"; 
+    var $password = "b681bd2d7"; 
+    var $dbname = "heroku_5b022ebec74d502"; 
+    var $conn;
+function getConnstring() { 
+    $con = mysqli_connect($this->servername, $this->username, $this->password, $this->dbname) or die("Connection failed: " . mysqli_connect_error()); 
+    if (mysqli_connect_errno()) {
+        printf("Connect failed: %s\n", mysqli_connect_error());
+        exit();
+    } else {
+        $this->conn = $con;
+    } 
+    return $this->conn;
+    }
+}
 ?>
